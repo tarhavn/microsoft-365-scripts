4b92a87c6d22730ebd68cef933c4d8c2b7103662
Gisle Tarhavn <gisle.tarhavn@outlook.com>
2025-08-08 10:45:20 +0200

    added connect-sharepoint-module snippet

---
+++ sharepoint/connect-sharepoint-online.ps1
@@ -0,0 +1,7 @@
+Get-SPOSite
+
+#Interactive MFA 
+$tenant = "contoso" #$user = "admin@contoso.onmicrosoft.com"
+Connect-SPOService -Url https://$tenant-admin.sharepoint.com
+
+#Disconnect-SPOService
